8c9d6303e719dcceaefb2235b9565f4afec8f17d
Your Name <yourmail@mail.com>
2019-07-20 16:36:35 +0800

    数据表生成，新建apps，迁移courses等到apps和fix bugs

--- jayq_Edu_Web/settings.py
+++ jayq_Edu_Web/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# 新建apps并把operation等移到apps中,需要告诉代码运行器路径
+sys.path.insert(0,os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -38,7 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "users"
+    "users",
+    "courses",
+    "organization",
+    "operation"
 ]
 # 重载方法 app名称+ class
 AUTH_USER_MODEL = "users.UserProfile"
@@ -112,18 +117,25 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+# 改为中文 django1.8前为zh-CN 1.8为zh-hans
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+# 更改时区
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+# USE_TZ = True
+# 把取为UTC时间（即国际时间）取消掉
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
